79de08c0868839ceb5b514e781e7a566a9fd5013
Kurt@Brunner.codes <Kurt@Brunner.codes>
2025-04-16 14:18:08 +0200

    mindmap list (just show)

--- ase-frontend/src/components/mindmap/MindmapList.tsx
+++ ase-frontend/src/components/mindmap/MindmapList.tsx
@@ -1,18 +1,29 @@
 import {GetCurrentUser} from "../../utils/storageWrapper.ts";
 import {useQuery} from "react-query";
 
+type Entry = {
+    id: number;
+    title: string;
+}
+type MindmapList = {
+    own: Entry[];
+    edit: Entry[];
+    read_only: Entry[];
+};
+
 function MindmapList() {
-    const user = GetCurrentUser();
-    const { isLoading, error, data } = useQuery({
-        queryKey: ['get_mindmap_list'],
+    const dateTime = new Date()
+    const { isLoading, error, data } = useQuery<MindmapList, Error>({
+        queryKey: [`get_mindmap_list_${dateTime.getMonth()}`],
         queryFn: async () => {
+            const user = GetCurrentUser();
             if (!user || !user.JWT) {
                 throw new Error("User not authenticated.");
             }
 
             const requestOptions = {
                 method: "GET",
-                headers: { Authorization: "Bearer " + user.JWT },
+                headers: { 'Authorization': 'Bearer ' + user.JWT },
             };
 
             const response = await fetch("http://localhost:3000/mindmap", requestOptions);
@@ -21,15 +32,24 @@ function MindmapList() {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
 
-            return response.json();
+            return await response.json() ?? 'No Data';
         }
     })
 
-    if (isLoading) return <p>loading</p>
+    if (isLoading || (data === undefined)) return <p>loading...</p>
 
-    if (error) return <p>error</p>;
+    if (error) return <p>{error?.message}</p>;
 
-    return <p>{JSON.stringify(data)}</p>
+    console.log(JSON.stringify(data, null, 2));
+
+    return <div>
+        <p><b>My Mindmaps</b></p>
+        {data.own.map((m : Entry) => (<p>{m.title}</p>))}
+        <p><b>Shared With Me</b></p>
+        {data.edit.map((m : Entry) => (<p>{m.title}</p>))}
+        <p><b>Read Only</b></p>
+        {data.read_only.map((m : Entry) => (<p>{m.title}</p>))}
+    </div>
 }
 
 export default MindmapList;
