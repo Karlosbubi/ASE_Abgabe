17d1c2c9993125f9937a339ac7b17d8a887517a2
Kurt@Brunner.codes <Kurt@Brunner.codes>
2025-04-18 14:33:59 +0200

    swagger for update DTO

--- ase-backend/src/types/dto/UpdateMindmapRights.ts
+++ ase-backend/src/types/dto/UpdateMindmapRights.ts
@@ -1,9 +1,14 @@
 import { Mindmap } from '../db_entities/mindmap';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateMindmapRights
 {
+  @ApiProperty()
   mindmap: number | Mindmap;
+  @ApiProperty()
   recipient_email: string;
+  @ApiProperty()
   can_read: boolean = false;
+  @ApiProperty()s
   can_write: boolean = false;
 }
