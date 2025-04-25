a2e4d29f480bb00e6faea56926d9186880c0e0fa
erikbuerge <erik.buerge@hotmail.com>
2025-04-25 15:40:20 +0200

    fix: toaster provided globally

--- ase-frontend/src/main.tsx
+++ ase-frontend/src/main.tsx
@@ -3,10 +3,15 @@ import { ReactFlowProvider } from "@xyflow/react";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ReactFlowProvider>
+            <Toaster
+                position="top-right"
+                containerStyle={{ top: '4.5rem' }} // verschiebt Toasts unter die NavBar
+            />
             <App />
         </ReactFlowProvider>
     </StrictMode>,
