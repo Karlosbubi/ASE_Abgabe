de968a8012bd4efe168f6bf86066df51296b2fe3
Kurt@Brunner.codes <Kurt@Brunner.codes>
2025-04-28 10:21:25 +0200

    revert: test: init db with example users

--- db_init/3-example_data.sql
+++
@@ -1,7 +0,0 @@
-\c postgres
-
-insert into mindmap_user(name, email, password, isadmin) VALUES
-    ('example_admin', 'admin@example.com', 'admin', true);
-
-insert into mindmap_user(name, email, password, isadmin) VALUES
-    ('example_user', 'user@example.com', 'user', false);
