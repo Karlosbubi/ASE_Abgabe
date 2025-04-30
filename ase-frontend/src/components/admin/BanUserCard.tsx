fe83a8321e2674214767bd6c49d40674e183edd6
Olaf4000 <daniel.luca.benfer@gmail.com>
2025-04-30 15:42:03 +0200

    unbanning of useres

--- ase-frontend/src/components/admin/BanUserCard.tsx
+++ ase-frontend/src/components/admin/BanUserCard.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import {GetCurrentUser} from "@/utils/storageWrapper.ts";
 import toast from "react-hot-toast";
+import {Button} from "@/components/ui/button.tsx";
 
 const BanUserCard = () => {
+    const [unbanMode, setUnbanMode] = useState(false);
+
+    const handleButton = () => {
+        setUnbanMode(!unbanMode);
+    }
+
     const user = GetCurrentUser();
     const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
         e.preventDefault();
         const userId = e.target.ID.value;
         const dto = {
             userId: Number(userId),
-            suspension: true,
+            suspension: !unbanMode,
         }
 
         const requestOptions = {
@@ -25,7 +32,7 @@ const BanUserCard = () => {
             const response = await fetch(`http://localhost:3000/admin/suspendUser`, requestOptions);
             if (response.ok) {
                 toast.success("User Suspended Successfully!");
-                //window.location.reload();
+                window.location.reload();
             }
         } catch (error) {
             toast.dismiss();
@@ -38,26 +45,80 @@ const BanUserCard = () => {
 
     return (
         <>
-            <div className="bg-white shadow-md rounded pt-6">
-                <div className="text-center font-bold text-lg mb-4">
-                    <h1>Ban User</h1>
-                </div>
 
-                <form onSubmit={handleSubmit} className="px-8 pt-6 pb-8 mb-4">
-                    <div className="flex items-center space-x-4">
-                        <input
-                            type="text" name="ID"
-                            className="shadow appearance-none border rounded w-full py-2 px-3
-                        text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            placeholder="User ID"/>
+                {!unbanMode ? (
+                    <div className="bg-white shadow-md rounded pt-6">
+                        <div className="text-center font-bold text-lg mb-4">
+                            <h1>Ban User</h1>
+                        </div>
+                        <div className="px-8 pt-6 pb-8 mb-4">
+                            <div className="flex items-center space-x-2">
+                                <form onSubmit={handleSubmit} className="flex items-center space-x-2">
+                                    <input
+                                        type="text"
+                                        name="ID"
+                                        className="shadow appearance-none border rounded py-2 px-3
+                   text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                        placeholder="User ID"
+                                    />
 
-                        <input type="submit" name="Submit" value="Ban" data-testid="ban-button"
-                               className="bg-blue-500 hover:bg-blue-700 text-white font-bold
-                           py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        />
+                                    <input
+                                        type="submit"
+                                        name="Submit"
+                                        value="Ban"
+                                        data-testid="ban-button"
+                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold
+                   py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                    />
+                                </form>
+
+                                <button
+                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold
+                 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                    onClick={handleButton}
+                                >
+                                    Change Mode
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                </form>
-            </div>
+                ) : (
+                    <div className="bg-white shadow-md rounded pt-6">
+                        <div className="text-center font-bold text-lg mb-4">
+                            <h1>Unban User</h1>
+                        </div>
+                        <div className="px-8 pt-6 pb-8 mb-4">
+                            <div className="flex items-center space-x-2">
+                                <form onSubmit={handleSubmit} className="flex items-center space-x-2">
+                                    <input
+                                        type="text"
+                                        name="ID"
+                                        className="shadow appearance-none border rounded py-2 px-3
+                   text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                        placeholder="User ID"
+                                    />
+
+                                    <input
+                                        type="submit"
+                                        name="Submit"
+                                        value="Unban"
+                                        data-testid="ban-button"
+                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold
+                   py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                    />
+                                </form>
+
+                                <button
+                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold
+                 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                    onClick={handleButton}
+                                >
+                                    Change Mode
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                )}
         </>
     )
 }
