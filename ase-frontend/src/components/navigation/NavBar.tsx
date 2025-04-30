1fa51ecb90fe72e8536fc9446b64d704bea98f0e
erikbuerge <erik.buerge@hotmail.com>
2025-04-30 14:35:18 +0200

    fix: Navbar "Admin" appearence

--- ase-frontend/src/components/navigation/NavBar.tsx
+++ ase-frontend/src/components/navigation/NavBar.tsx
@@ -4,14 +4,22 @@ import {ClearCurrentUser, GetCurrentUser} from "@/utils/storageWrapper.ts";
 
 const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const user = GetCurrentUser();
+    console.log(user?.isAdmin);
 
-    if(GetCurrentUser() === null) {
-        return ( //Navbar if user is not logged in
+    if(user){
+        return (
             <nav className="fixed top-0 left-0 w-full bg-white shadow-md p-4 z-50">
                 <div className="container mx-auto flex justify-between items-center">
                     <a className="text-xl font-bold" href="/">Mind Board</a>
                     <div className="hidden md:flex space-x-6">
-                        <a href="/login" className="hover:text-blue-600"> Login </a>
+                        <a data-testid="dashboard-link" href="/dashboard"
+                           className="hover:text-blue-600"> Dashboard </a>
+                        {user.isAdmin && (
+                            <a href="/admin" className="hover:text-blue-600"> Admin </a>
+                        )}
+                        <a href="/" className="hover:text-blue-600" onClick={ClearCurrentUser}
+                           data-testid="logout-link"> Logout </a>
                     </div>
                     <button
                         className="md:hidden p-2 rounded bg-gray-200"
@@ -22,38 +30,36 @@ const NavBar = () => {
                 {isOpen && (
                     <div className="md:hidden flex flex-col items-center space-y-4 mt-4">
                         <a href="/wip" className="hover:text-blue-600"> Import </a>
-                        <a href="/login" className="hover:text-blue-600"> Login </a>
-                    </div>
-                )}
-            </nav>
-        );
-    } else {
-        return ( //Navbar if user is logged in
-            <nav className="fixed top-0 left-0 w-full bg-white shadow-md p-4 z-50">
-                <div className="container mx-auto flex justify-between items-center">
-                    <a className="text-xl font-bold" href="/">Mind Board</a>
-                    <div className="hidden md:flex space-x-6">
-                        <a data-testid="dashboard-link" href="/dashboard" className="hover:text-blue-600"> Dashboard </a>
-                        <a href="/admin" className="hover:text-blue-600"> Admin </a> {/*//TODO: meh*/}
-                        <a href="/" className="hover:text-blue-600" onClick={ClearCurrentUser} data-testid="logout-link"> Logout </a>
-                    </div>
-                    <button
-                        className="md:hidden p-2 rounded bg-gray-200"
-                        onClick={() => setIsOpen(!isOpen)}>
-                        {isOpen ? <X/> : <Menu/>}
-                    </button>
-                </div>
-                {isOpen && (
-                    <div className="md:hidden flex flex-col items-center space-y-4 mt-4">
-                        <a href="/wip" className="hover:text-blue-600"> Import </a>
-                        <a data-testid="dashboard-link" href="/dashboard" className="hover:text-blue-600"> Dashboard </a>
+                        <a data-testid="dashboard-link" href="/dashboard"
+                           className="hover:text-blue-600"> Dashboard </a>
                         <a href="/" className="hover:text-blue-600" onClick={ClearCurrentUser}> Logout </a>
                     </div>
                 )}
             </nav>
-        );
+        )
+    } else {
+        return (
+            <nav className="fixed top-0 left-0 w-full bg-white shadow-md p-4 z-50">
+                <div className="container mx-auto flex justify-between items-center">
+                    <a className="text-xl font-bold" href="/">Mind Board</a>
+                    <div className="hidden md:flex space-x-6">
+                        <a href="/login" className="hover:text-blue-600"> Login </a>
+                    </div>
+                    <button
+                        className="md:hidden p-2 rounded bg-gray-200"
+                        onClick={() => setIsOpen(!isOpen)}>
+                        {isOpen ? <X/> : <Menu/>}
+                    </button>
+                </div>
+                {isOpen && (
+                    <div className="md:hidden flex flex-col items-center space-y-4 mt-4">
+                        <a href="/wip" className="hover:text-blue-600"> Import </a>
+                        <a href="/login" className="hover:text-blue-600"> Login </a>
+                    </div>
+                )}
+            </nav>
+        )
     }
-
 };
 
 export default NavBar;
